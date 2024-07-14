0b9f69acff6f6dd5db7fac1a88ae22e6d44378ae
Gabriel Isaac <stewartisaac90@gmail.com>
2024-07-14 01:59:50 +0100

    add comments

--- quiz.js
+++ quiz.js
@@ -1,12 +1,18 @@
+// Define Function
 function checkAnswer() {
-    let correctAnswer = "4";
+    let correctAnswer = "4"; // Declare variable and assign value
+    
+    // select the checked radio button by its name attribute
     let userAnswer = document.querySelector("input[name=quiz]:checked").value;
+    
+    // check if the user’s answer is correct
     if(userAnswer == correctAnswer){
         document.getElementById('feedback').textContent = "Correct! Well done.";
-    } else {
+    } else { // if incorrect, display a different message
         document.getElementById('feedback').textContent = "That's incorrect. Try again!";
     }
 };
 
+// Add a click event listener to the submit answer button
 document.getElementById("submit-answer").addEventListener("click", checkAnswer);
 
